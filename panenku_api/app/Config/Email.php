91cb83992a8f0431466a5c2c6176a75435fac1bd
AhmadShofii <ahmadshofii023@gmail.com>
2026-07-29 00:53:54 +0800

    chore: backup before migrating to Shield

--- panenku_api/app/Config/Email.php
+++ panenku_api/app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'ahmadshofii023@gmail.com';
+    public string $fromName   = 'panenku';
     public string $recipients = '';
 
     /**
